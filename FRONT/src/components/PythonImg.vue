e55fa6e3d34c1d79413f6447a63058ae4a37ae03
GRINTKIM <bosung2451@gmail.com>
2022-09-13 09:04:47 +0900

    commit CNN part

--- FRONT/src/components/PythonImg.vue
+++ FRONT/src/components/PythonImg.vue
@@ -3,9 +3,12 @@
     <h1>{{ msg }}</h1>
     <!-- <p>Title : {{title}}</p> -->
     <!-- Plain mode -->
-    <b-form-file v-model="file" class="mt-3" plain></b-form-file>
+    <b-form-file v-model="file" class="mt-3" plain @change="previewImage"></b-form-file>
     <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div><br />
-    <button @click.stop="apievent">요청</button><br /><br />
+    <div>
+      <b-img :src="previewImageData"></b-img>
+    </div><br />
+    <button @click.stop="apievent()">요청</button><br /><br /><br /><br />
     <h3>Api 요청 결과 값 : {{ apiresult }}</h3>
   </div>
 </template>
@@ -20,15 +23,18 @@ export default {
     return {
       msg: 'Digit Image Upload & Inference',
       apiresult: 'Waiting',
-      file: null
+      file: null,
+      show: true,
+      previewImageData: null
     }
   },
   methods: {
     async apievent () {
       const formdata = new FormData()
       formdata.append('file', this.file)
+      console.log('들어옴')
 
-      const { data } = await axios.post(
+      const {data} = await axios.post(
         'http://localhost:3010/api/pythonGet/imgUpload',
         formdata,
         {
@@ -37,9 +43,22 @@ export default {
           }
         })
       console.log(data)
-
-      // this.apiresult = this.file.name
+      this.apiresult = data
       // console.log(this.file)
+    },
+
+    previewImage (evt) {
+      var input = event.target
+
+      if (input.files && input.files[0]) {
+        var reader = new FileReader()
+        reader.onload = e => {
+          this.previewImageData = e.target.result
+        }
+        reader.readAsDataURL(input.files[0])
+      } else {
+        this.previewImageData = null
+      }
     }
   }
 }
