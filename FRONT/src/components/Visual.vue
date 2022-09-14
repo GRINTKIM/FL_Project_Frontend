bd3d9b6a6358c3cf016ac08ce79a22e6815cb6f6
GRINTKIM <bosung2451@gmail.com>
2022-09-14 17:17:57 +0900

    Data Visualization part

--- FRONT/src/components/Visual.vue
+++ FRONT/src/components/Visual.vue
@@ -1,52 +1,43 @@
-<!-- <template>
-    <div>
-        <h1>{{ msg }}</h1>
-          <select v-model="selected" multiple>
-              <option :value="{name:'A'}">A</option>>
-              <option :value="{name:'B'}">B</option>>
-              <option :value="{name:'D'}">D</option>>
-              <option :value="{name:'E'}">E</option>>
-              <option :value="{name:'G'}">G</option>>
-              <option :value="{name:'O'}">O</option>>
-              </select><br /><br />
-          <button @click.stop="apievent">요청</button>
-        <h2>Api 요청 결과 값 : {{apiresult}}</h2>
-
-    </div>
-</template> --> 수정 전
-
 <template>
     <div>
       <h1>{{ msg }}</h1>
         <b-form-select v-model="selected" :options="options"></b-form-select>
         <button @click.stop="apievent">요청</button>
-        <div class="container">
-          <Bar v-if="loaded==true" :chart-data="chartData" />
-        </div>
-        <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+        <GChart type="ColumnChart" :data="chartData" :options="chartOptions" />
+        <div class="mt-3">Selected: <strong>{{ selected }}</strong></div><br  />
       <!-- <h3>Result :<br /> {{apiresult}}</h3> -->
-      {{apiresult}}
+      Result: {{apiresult}}
     </div>
 </template>
 
 <script>
 import axios from 'axios'
-import { Bar } from 'vue-chartjs'
-import Chart from 'chart.js'
-Chart.pluginService.register()
-// ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+// import { GChart } from 'vue-google-charts'
+import { GChart } from 'vue-google-charts/legacy'
 
 export default {
-  name: 'BarChart',
-  extends: Bar,
-  components: { Bar },
+  name: 'Visual',
+  components: { GChart },
   data () {
     return {
       msg: 'Visual Data',
       apiresult: 'Waiting',
-      loaded: false,
-      chartData: null,
+      // loaded: false,
       selected: null,
+      chartData: null,
+      // chartData: [
+      //   ['Year', 'Sales', 'Expenses', 'Profit'],
+      //   ['2014', 1000, 400, 200],
+      //   ['2015', 1170, 460, 250],
+      //   ['2016', 660, 1120, 300],
+      //   ['2017', 1030, 540, 350]
+      // ],
+      chartOptions: {
+        chart: {
+          title: 'Company Performance',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017'
+        }
+      },
       options: [
         { value: null, text: 'COMP_ID' },
         { value: 'A', text: 'A' },
@@ -56,16 +47,6 @@ export default {
         { value: 'G', text: 'G' },
         { value: 'O', text: 'O' }
       ]
-      // props: ['chartdata',
-      //   {
-      //     scales: {
-      //       yAxes: [{ ticks: { beginAtZero: true }, gridLines: { display: true } }],
-      //       xAxes: [{ gridLines: { display: false } }]
-      //     },
-      //     legend: { display: true },
-      //     responsive: true,
-      //     maintainAspectRatio: false
-      //   }]
     }
   },
   methods: {
@@ -73,19 +54,23 @@ export default {
       let compselected = {comp: this.selected}
       console.log('Selected COMP_ID is ', this.selected)
       if (this.selected != null) {
-        const {data} = await axios.post('http://localhost:3010/api/Visual/comp/', compselected)
+        const { data } = await axios.post('http://localhost:3010/api/Visual/comp/', compselected)
         this.apiresult = data
-        this.loaded = true
+        const dt = [['DATE_T', 'TOTALCNT']]
+
+        for (var i = 1; i < data.length; i++) {
+          dt[i] = []
+          dt[i].push(data[i].DATE_T)
+          dt[i].push(Number(data[i].TOTALCNT))
+        }
+
+        console.log('dt: \n', dt)
+        this.chartData = dt
       } else {
         this.apiresult = 'Please select COMP_ID to satisfy WHERE clause.'
-        this.loaded = false
       }
-      this.chartData = this.apiresult
-      console.log(this.loaded)
-      console.log('charData: ', this.chartData)
-      // const {data} = await axios.post('http://localhost:3010/api/Visual/comp/', compselected)
-      // // console.log(data)
-      // this.apiresult = data
+      // console.log('chartData: ', this.dt)
+      // console.log(this.apiresult)
     }
   }
 }
